205154a99e726c3e7d1052c48902663f2818bde5
yurico <yuricowp1@gmail.com>
2025-06-22 18:35:26 +0700

    Penambahan Photo

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -18,7 +18,7 @@ class ProductSeeder extends Seeder
                 'sell_price' => 45000,
                 'cost_price' => 30000,
                 'profit' => 15000,
-                'image' => 'https://via.placeholder.com/300x200?text=Brownies+Coklat',
+                'image' => 'https://png.pngtree.com/png-clipart/20231021/original/pngtree-watercolor-brownie-chocolate-png-image_13391742.png',
                 'description' => 'Brownies coklat lembut dan legit, cocok untuk camilan keluarga.',
                 'created_at' => now(), 'updated_at' => now()
             ],
@@ -30,7 +30,7 @@ class ProductSeeder extends Seeder
                 'sell_price' => 65000,
                 'cost_price' => 45000,
                 'profit' => 20000,
-                'image' => 'https://via.placeholder.com/300x200?text=Cheesecake+Stroberi',
+                'image' => 'https://th.bing.com/th/id/OIP.TOGfGDGu4-Ct6vpKBMY1vAHaHa?w=179&h=180&c=7&r=0&o=7&pid=1.7&rm=3',
                 'description' => 'Cheesecake dingin dengan topping stroberi segar.',
                 'created_at' => now(), 'updated_at' => now()
             ],
@@ -42,7 +42,7 @@ class ProductSeeder extends Seeder
                 'sell_price' => 50000,
                 'cost_price' => 35000,
                 'profit' => 15000,
-                'image' => 'https://via.placeholder.com/300x200?text=Tiramisu+Slice',
+                'image' => 'https://static.vecteezy.com/system/resources/previews/054/809/640/large_2x/tiramisu-slice-isolated-on-transparent-background-png.png',
                 'description' => 'Potongan kue tiramisu dengan lapisan krim dan kopi.',
                 'created_at' => now(), 'updated_at' => now()
             ],
